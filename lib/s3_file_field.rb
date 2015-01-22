20b2fff63181a0913c62828514a1a9b84a46ab6a
Patrick Roy <patroy@gmail.com>
2015-01-22 15:17:58 -0400

    Don't use JQuery::FileUpload::Rails::Middleware

--- lib/s3_file_field.rb
+++ lib/s3_file_field.rb
@@ -9,7 +9,7 @@ require 'active_support/core_ext'
 require 's3_file_field/config_aws'
 require 's3_file_field/s3_uploader'
 require 's3_file_field/form_helper'
-require 's3_file_field/railtie'
+# require 's3_file_field/railtie'
 require 's3_file_field/engine'
 
 ActionView::Base.send(:include, S3FileField::FormHelper)
